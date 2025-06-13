874763f019052059d1b43268c766ef60b6705b6c
Jogatev <142318568+Jogatev@users.noreply.github.com>
2025-06-13 10:26:43 +0800

    notw

--- NMB/app/build.gradle.kts
+++ NMB/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kapt) // Required for Hilt
+    alias(libs.plugins.hilt) // Apply the Hilt plugin
 }
 
 android {
@@ -27,45 +29,49 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
     }
 }
 
 dependencies {
-    implementation("javax.inject:javax.inject:1")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
+    implementation("javax.inject:javax.inject:1")
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.car.ui.lib)
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation("com.google.dagger:hilt-android:2.51")
+    kapt("com.google.dagger:hilt-compiler:2.51")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     implementation(libs.androidx.room.runtime.android)
+    implementation("io.insert-koin:koin-android:3.5.3")
+    implementation(libs.car.ui.lib)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation ("androidx.hilt:hilt-navigation-compose:1.1.0")
-    implementation ("androidx.compose.material3:material3:1.2.0")
-    implementation("androidx.compose.material3:material3:<latest_version>")
-    implementation("androidx.navigation:navigation-compose:<latest_version>")
-    implementation ("androidx.compose.material:material-icons-extended:")
-    implementation ("androidx.compose.material:material-icons-extended:1.5.4")
-    implementation ("io.insert-koin:koin-android:3.5.3")
-    implementation ("com.google.code.gson:gson:2.10.1")
 }
